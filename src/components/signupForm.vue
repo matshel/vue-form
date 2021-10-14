8272450074c9a29e70d306e2149f194fb74c05f3
matshel <mats.helgesen@protonmail.com>
2021-10-14 17:37:52 +0200

    Update 2 way data binding

--- src/components/signupForm.vue
+++ src/components/signupForm.vue
@@ -1,10 +1,24 @@
-<script></script>
+<script>
+	export default {
+		data() {
+			return {
+				email: "",
+				password: "",
+			};
+		},
+	};
+</script>
 
 <template>
 	<form>
 		<label>Email:</label>
-		<input type="email" required />
+		<input type="email" required v-model="email" />
+
+		<label>Password:</label>
+		<input type="password" required v-model="password" />
 	</form>
+	<p>Email: {{ email }}</p>
+	<p>Password: {{ password }}</p>
 </template>
 
 <style>
