bdffadc6248ccb5820f63d901161330071ed1459
matshel <mats.helgesen@protonmail.com>
2021-10-14 17:28:15 +0200

    general update

---
+++ src/components/signupForm.vue
@@ -0,0 +1,36 @@
+<script></script>
+
+<template>
+	<form>
+		<label>Email:</label>
+		<input type="email" required />
+	</form>
+</template>
+
+<style>
+	form {
+		max-width: 420px;
+		margin: 30px auto;
+		padding: 40px;
+		background-color: #fff;
+		text-align: left;
+		border-radius: 10px;
+	}
+	label {
+		display: inline-block;
+		color: #aaa;
+		margin: 25px 0 15px;
+		font-size: 0.6rem;
+		text-transform: uppercase;
+		letter-spacing: 1px;
+		font-weight: bold;
+	}
+	input {
+		display: block;
+		padding: 10px 6px;
+		width: 100%;
+		border: none;
+		border-bottom: 1px solid #ddd;
+		color: #555;
+	}
+</style>
